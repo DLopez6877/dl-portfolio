36084b5bfd3c14a4e9bb31c8d4af3c47751cfd1e
Daniel Lopez <dlopez6877@gmail.com>
2017-09-22 22:31:16 -0700

    add background color transitions

--- js/projects-interface.js
+++ js/projects-interface.js
@@ -6,6 +6,7 @@ $(function() {
       $('#project1').show();
       $('.featured-project-image').html('<img src="../assets/images/devices1.png">');
       $('.featured-image').css('background-image', 'url(../../assets/images/robot-siyan-ren.jpeg)')
+      $('body').css('background', '#000');
   });
   $('.page-link2').click(function() {
       $('.page-link').removeClass('active-page-link');
@@ -14,6 +15,7 @@ $(function() {
       $('#project2').show();
       $('.featured-project-image').html('<img src="../assets/images/devices2.png">');
       $('.featured-image').css('background-image', 'url(../../assets/images/mike-dorner.jpg)')
+      $('body').css('background', '#2c87a5');
   });
   $('.page-link3').click(function() {
       $('.page-link').removeClass('active-page-link');
@@ -22,6 +24,7 @@ $(function() {
       $('#project3').show();
       $('.featured-project-image').html('<img src="../assets/images/devices3.png">');
       $('.featured-image').css('background-image', 'url(../../assets/images/freestocks-org.jpg)')
+      $('body').css('background', '#004380');
   });
   $('.page-link4').click(function() {
       $('.page-link').removeClass('active-page-link');
@@ -30,6 +33,7 @@ $(function() {
       $('#project4').show();
       $('.featured-project-image').html('<img src="../assets/images/devices4.png">');
       $('.featured-image').css('background-image', 'url(../../assets/images/bart-larue.jpg)')
+      $('body').css('background', '#0a0721');
   });
   $('.page-link5').click(function() {
       $('.page-link').removeClass('active-page-link');
@@ -37,5 +41,7 @@ $(function() {
       $('.feature-wrapper').hide();
       $('#project5').show();
       $('.featured-project-image').html('<img src="../assets/images/devices5.png">');
+      $('.featured-image').css('background-image', 'url(../../assets/images/bart-larue.jpg)')
+      $('body').css('background', '#004380');
   });
 });
