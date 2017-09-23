5b6fca18af8f954fbcb776ca35d1843d4f707a1e
Daniel Lopez <dlopez6877@gmail.com>
2017-09-23 10:03:34 -0700

    add preloading

--- js/featured-project-image-interface.js
+++ js/featured-project-image-interface.js
@@ -12,4 +12,19 @@ $(function() {
     }
     $(this).html('<img src="../assets/images/devices'+ number + '.png">');
   });
+
+  var images = new Array()
+			function preload() {
+				for (i = 0; i < preload.arguments.length; i++) {
+					images[i] = new Image()
+					images[i].src = preload.arguments[i]
+				}
+			}
+			preload(
+				"../../assets/images/robot-siyan-ren.jpeg",
+				"../../assets/images/mike-dorner.jpg",
+				"../../assets/images/freestocks-org.jpg",
+				"../../assets/images/bart-larue.jpg",
+				"../../assets/images/daniel-cheung.jpg"
+			)
 });
