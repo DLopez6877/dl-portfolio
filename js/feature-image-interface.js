e220ce26734e2c45b723ebeee83cb4ff3220faad
Daniel Lopez <dlopez6877@gmail.com>
2017-09-23 21:52:57 -0700

    add additional preload images and padding to about container

--- js/feature-image-interface.js
+++ js/feature-image-interface.js
@@ -17,6 +17,11 @@ $(function() {
 		"../../assets/images/mike-dorner.jpg",
 		"../../assets/images/freestocks-org.jpg",
 		"../../assets/images/bart-larue.jpg",
-		"../../assets/images/daniel-cheung.jpg"
+		"../../assets/images/daniel-cheung.jpg",
+    "../../assets/images/devices1.png",
+    "../../assets/images/devices2.png",
+    "../../assets/images/devices3.png",
+    "../../assets/images/devices4.png",
+    "../../assets/images/devices5.png"
 	)
 });
