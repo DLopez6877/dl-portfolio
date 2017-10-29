187cc6ad77d762e7c0e1f5761ab3eedabecaa88c
Daniel Lopez <dlopez6877@gmail.com>
2017-10-29 02:22:57 -0700

    remove image links. add content to tldr

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -36,22 +36,6 @@ $(function() {
 });
 
 $(function() {
-  $('.image-link').click(function() {
-    $('.featured-project-image').html($(this).html());
-  });
-
-  $('.featured-project-image').click( function() {
-    var number;
-    for (var i = 1; i <= 5; i++) {
-      if ($(this).hasClass('project' + i))  {
-        number = i;
-      }
-    }
-    $(this).html('<img src="/assets/images/devices'+ number + '.png">');
-  });
-});
-
-$(function() {
   // About Me
   $('.about-nav').click(function() {
     $('#project-container').hide();
