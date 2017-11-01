6ddabddd119d9e91255a611b0eb02852761d6e1f
Daniel Lopez <dlopez6877@gmail.com>
2017-10-31 18:26:30 -0700

    change about and contact

--- js/nav-interface.js
+++ js/nav-interface.js
@@ -2,17 +2,19 @@ $(function() {
   // About Me
   $('.about-nav').click(function() {
     $('#project-container').hide();
+    $('#contact-container').hide();
     $('#about-container').show();
   });
   // Projects
   $('.projects-nav').click(function() {
     $('#about-container').hide();
+    $('#contact-container').hide();
     $('#project-container').show();
   });
   // Contact
   $('.contact-nav').click(function() {
-    $('.contact-info').slideToggle("slow", "swing", function(){
-      console.log('¯\\_(ツ)_/¯ email me?');
-    });
+    $('#about-container').hide();
+    $('#project-container').hide();
+    $('#contact-container').show();
   });
 });
